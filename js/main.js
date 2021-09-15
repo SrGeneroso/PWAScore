1fa7f3dc80dab1167bbb6b7cb779d7a243dac22b
SrGeneroso <sr.generoso@gmail.com>
2021-09-16 00:20:55 +0200

    first look

--- js/main.js
+++ js/main.js
@@ -53,3 +53,35 @@ function vaildateCacheIfOnline(){
 
 }
 
+function horzScrollbarDetect() {
+  
+    var $scrollable = $('.scrollable')
+    var $innerDiv = $('.scrollable > div');
+    
+    if ($innerDiv.outerWidth() < $innerDiv.get(0).scrollWidth) {
+      
+      $scrollable.addClass('is-scrollable');
+      console.log('Scrollbar, WOOT!')
+      
+    } else {
+      
+      $scrollable.removeClass('is-scrollable');
+      console.log('There is no scrollbar, only Zuul');
+      
+    }
+    
+  }
+  
+  $(document).ready(function() {
+    
+    horzScrollbarDetect();
+    console.log('document. boom. ready.')
+    
+  });
+  
+  $(window).resize(function() {
+    
+    horzScrollbarDetect();
+    console.log('window resized');
+    
+  });
