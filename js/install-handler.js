12771014013784dd97acc30041b5c284a8ed5d2e
SrGeneroso <sr.generoso@gmail.com>
2021-09-16 03:46:29 +0200

    added js

--- js/install-handler.js
+++ js/install-handler.js
@@ -1,19 +1,19 @@
 var deferredInstallPrompt = null;
 
 
-window.addEventListener('beforeinstallprompt', function (event) {
+window.addEventListener('beforeinstallprompt', function(event) {
     event.preventDefault();
     deferredInstallPrompt = event;
     showDownloadPrompt();
 });
 
 
-document.querySelector('.downloadButton').addEventListener('click', downloadButtonClicked)
+// document.querySelector('.downloadButton').addEventListener('click', downloadButtonClicked)
 
 function downloadButtonClicked() {
     deferredInstallPrompt.prompt();
     deferredInstallPrompt.userChoice
-        .then(function (choiceResult) {
+        .then(function(choiceResult) {
             if (choiceResult.outcome === 'accepted') {
 
                 deferredInstallPrompt = null;
@@ -31,27 +31,24 @@ function showDownloadPrompt() {
 
 window.addEventListener('appinstalled', (evt) => {
     // Log install to analytics
-    
+
     if (!isInStandaloneMode()) {
         alert('open in app');
     }
 });
 
-async function foo(){
+async function foo() {
     if ('getInstalledRelatedApps' in window.navigator) {
         const relatedApps = await navigator.getInstalledRelatedApps();
         relatedApps.forEach((app) => {
-          //if your PWA exists in the array it is installed
-          alert(app.platform, app.url);
+            //if your PWA exists in the array it is installed
+            alert(app.platform, app.url);
         });
-      }
+    }
 }
 
 foo();
 
 
 
-const isInStandaloneMode = () =>(window.matchMedia('(display-mode: standalone)').matches) || (window.navigator.standalone) || document.referrer.includes('android-app://');
-
-
-
+const isInStandaloneMode = () => (window.matchMedia('(display-mode: standalone)').matches) || (window.navigator.standalone) || document.referrer.includes('android-app://');
--- js/main.js
+++ js/main.js
@@ -1,87 +1,178 @@
+window.addEventListener('load', main)
+window.addEventListener('resize', horzScrollbarDetect);
+// document.getElementById('scoreTable').addEventListener('scroll', scrollTableShadow);
 
+function main() {
+    vaildateCacheIfOnline().then(_ => {});
 
-
-window.addEventListener('load',main)
-function main(){
-
-    vaildateCacheIfOnline()
-    .then(_=>{
-        
-    })
-
+    horzScrollbarDetect();
 }
 
+function vaildateCacheIfOnline() {
 
-
-
-function vaildateCacheIfOnline(){
-
-    return new Promise((resolve,reject)=>{
+    return new Promise((resolve, reject) => {
 
         fetch(`config.json?cacheBust=${new Date().getTime()}`)
-        .then(response => { return response.json() })
-        .then(config => {
+            .then(response => { return response.json() })
+            .then(config => {
 
-            let installedVersion = Settings.getVersion()
-            if ( installedVersion== 0) {
-                Settings.setVersion(config.version)
-                document.querySelector('#version').innerHTML= `version ${config.version}`;
-                return resolve();
-            }
-            else if (installedVersion != config.version) {
-                console.log('Cache Version mismatch')
-                fetch(`config.json?clean-cache=true&cacheBust=${new Date().getTime()}`).then(_ => {
-                    //actually cleans the cache
-                    Settings.setVersion(config.version);
-                    window.location.reload();
-                   
-                    return resolve();  // unnecessary
-                });
+                let installedVersion = Settings.getVersion()
+                if (installedVersion == 0) {
+                    Settings.setVersion(config.version)
+                    document.querySelector('#version').innerHTML = `version ${config.version}`;
+                    return resolve();
+                } else if (installedVersion != config.version) {
+                    console.log('Cache Version mismatch')
+                    fetch(`config.json?clean-cache=true&cacheBust=${new Date().getTime()}`).then(_ => {
+                        //actually cleans the cache
+                        Settings.setVersion(config.version);
+                        window.location.reload();
 
-            }else{
-                // already updated
-                console.log('Cache Updated')
-                document.querySelector('#version').innerHTML= `version ${installedVersion}`;
+                        return resolve(); // unnecessary
+                    });
+
+                } else {
+                    // already updated
+                    console.log('Cache Updated')
+                    return resolve();
+                }
+            }).catch(err => {
+                console.log(err);
                 return resolve();
-            }
-        }).catch(err=>{
-            console.log(err);
-            return resolve();
-            //handle offline here 
-        })
+                //handle offline here 
+            })
     })
 
 }
 
+
 function horzScrollbarDetect() {
-  
-    var $scrollable = $('.scrollable')
-    var $innerDiv = $('.scrollable > div');
-    
-    if ($innerDiv.outerWidth() < $innerDiv.get(0).scrollWidth) {
-      
-      $scrollable.addClass('is-scrollable');
-      console.log('Scrollbar, WOOT!')
-      
+
+    var scrollable = document.querySelector('.scrollable');
+    var innerDiv = document.querySelector('.scrollable > div');
+
+    if (innerDiv.clientWidth < innerDiv.scrollWidth) {
+        scrollable.classList.add('is-scrollable');
+        // console.log('Scrollbar, WOOT!')
     } else {
-      
-      $scrollable.removeClass('is-scrollable');
-      console.log('There is no scrollbar, only Zuul');
-      
+        scrollable.classList.remove('is-scrollable');
+        // console.log('There is no scrollbar, only Zuul');
     }
-    
-  }
-  
-  $(document).ready(function() {
-    
-    horzScrollbarDetect();
-    console.log('document. boom. ready.')
-    
-  });
-  
-  $(window).resize(function() {
-    
-    horzScrollbarDetect();
-    console.log('window resized');
-    
-  });
+}
+
+var currentPlayerSelected;
+var player1 = document.getElementById("player1CurrentScore");
+var player2 = document.getElementById("player2CurrentScore");
+var player3 = document.getElementById("player3CurrentScore");
+var player1Table = document.getElementById("tablePlayer1");
+var player2Table = document.getElementById("tablePlayer2");
+var player3Table = document.getElementById("tablePlayer3");
+var buttons = document.querySelector('.currentPlayerScore');
+
+function selectPlayer(idPlayer) {
+
+    buttons.classList.remove("grey", "yellow", "red", "blue");
+
+    switch (idPlayer) {
+        case 0:
+            currentPlayerSelected = player1;
+            buttons.classList.add("yellow");
+            break;
+        case 1:
+            currentPlayerSelected = player2;
+            buttons.classList.add("red");
+            break;
+        case 2:
+            currentPlayerSelected = player3;
+            buttons.classList.add("blue");
+            break;
+    }
+}
+
+function addScoreToPlayer(scoreValue) {
+    if (currentPlayerSelected == null) {
+        return
+    }
+
+    currentPlayerSelected.innerHTML = parseInt(currentPlayerSelected.innerHTML) + scoreValue;
+
+}
+
+function submitPlayerScore(submitType) {
+    if (currentPlayerSelected == null) {
+        return
+    }
+
+    buttons.classList.remove("yellow", "red", "blue");
+    buttons.classList.add("grey");
+
+    switch (submitType) {
+        case 0:
+            currentPlayerSelected.innerHTML = currentPlayerSelected.innerHTML * -1;
+            currentPlayerSelected = undefined;
+            break;
+        case 1:
+            currentPlayerSelected.innerHTML = 0;
+            currentPlayerSelected = undefined;
+            break;
+        case 2:
+            currentPlayerSelected.innerHTML = currentPlayerSelected.innerHTML * 1;
+            currentPlayerSelected = undefined;
+            break;
+    }
+}
+
+function submitRound() {
+    let player1RoundScore = document.createElement('td');
+    let player2RoundScore = document.createElement('td');
+    let player3RoundScore = document.createElement('td');
+
+    buttons.classList.remove("yellow", "red", "blue");
+    buttons.classList.add("grey");
+
+    player1RoundScore.innerHTML = parseInt(player1.innerHTML) + parseInt(player1Table.lastElementChild.textContent);
+    player1.innerHTML = 0;
+    player1Table.appendChild(player1RoundScore);
+
+    player2RoundScore.innerHTML = parseInt(player2.innerHTML) + parseInt(player2Table.lastElementChild.textContent);
+    player2.innerHTML = 0;
+    player2Table.appendChild(player2RoundScore);
+
+    player3RoundScore.innerHTML = parseInt(player3.innerHTML) + parseInt(player3Table.lastElementChild.textContent);
+    player3.innerHTML = 0;
+    player3Table.appendChild(player3RoundScore);
+}
+
+
+/* Try to get shadows on both sides */
+// function scrollTableShadow() {
+
+// //poor code here
+// let table = document.getElementById('scoreTable')
+// let scrollCurrent = table.scrollLeft;
+// let scrollMax = table.scrollWidth - table.clientWidth;
+// let scrollable = document.querySelector('.scrollable');
+
+
+
+// console.log(table.scrollWidth + "scrollwidth")
+// console.log(table.scrollLeft + "scrollLeft")
+// console.log(table.clientWidth + "clientWidth")
+// console.log(scrollCurrent + "scrollCurrent")
+// console.log(scrollMax + "scrollMax")
+// if (scrollCurrent == 0) {
+//     scrollable.id = "shadowLeft"
+// } else if (scrollCurrent >= scrollMax) {
+//     scrollable.id = "shadowRight"
+// } else {
+//     scrollable.id = "shadowBoth"
+// }
+
+// if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     // document.getElementById("myP").className = "test";
+//     console.log('hey')
+// } else {
+//     document.getElementById("myP").className = "";
+//     console.log('heeeey')
+// }
+// }
